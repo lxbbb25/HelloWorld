38ce3f53b1bf01eec53c82825675a16f132f74e5
liuxb <liuxb@mail.las.ac.cn>
2020-10-13 00:35:12 +0800

    Integrate ThymeLeaf

--- src/main/java/com/lxb/controller/PageController.java
+++ src/main/java/com/lxb/controller/PageController.java
@@ -1,13 +1,16 @@
 package com.lxb.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PageController {
 
 	@RequestMapping("/index")
-	public String index() {
+	public String index(Model model) {
+		model.addAttribute("name", "刘小兵");
+		
 		return "index";
 	}
 }
