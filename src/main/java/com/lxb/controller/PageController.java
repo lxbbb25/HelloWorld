5c98a47fcd14e5907d456831ee5da00adb7b62a5
liuxb <liuxb@mail.las.ac.cn>
2020-10-14 01:59:51 +0800

    thymeleaf and mybatis config in pom.xml

--- src/main/java/com/lxb/controller/PageController.java
+++ src/main/java/com/lxb/controller/PageController.java
@@ -1,5 +1,8 @@
 package com.lxb.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +12,12 @@ public class PageController {
 
 	@RequestMapping("/index")
 	public String index(Model model) {
+		List<String> list = new ArrayList();
+		list.add("A");
+		list.add("B");
+		list.add("C");
+		
+		model.addAttribute("list", list);
 		model.addAttribute("name", "刘小兵");
 		
 		return "index";
